329b3252822c9586b3b9f7ec95d2be30dae68bcc
Aldo <ernestoo.pc@gmail.com>
2025-03-02 19:31:19 -0500

    refactor: corrections in components product listing

--- src/app/listado-productos/listado-productos.component.ts
+++ src/app/listado-productos/listado-productos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Producto } from './model/producto-model';
+import { ProductoComponent } from './producto/producto.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listado-productos',
-  imports: [],
+  imports: [ProductoComponent,FormsModule],
   templateUrl: './listado-productos.component.html',
   styleUrl: './listado-productos.component.css'
 })
@@ -17,11 +19,23 @@ export class ListadoProductosComponent {
 
   ];
 
-  agregarProducto(description: string,precio:number):void{
-    if(description && precio){
-      const nuevoProducto = new Producto(description,precio);
-      this.productos.push(nuevoProducto);
+  descripcionInput:string = '';
+  precioInput: number | null = null;
+
+  agregarProducto(){
+    if(this.descripcionInput.trim() == '' || this.precioInput == null || this.precioInput <=0){
+      console.log("Debe ingresar una descripción y un precio validos")
+      return;
     }
+
+    const producto = new Producto(this.descripcionInput,this.precioInput);
+    this.productos.push(producto);
+
+    //Limpiamos los campos del formulario
+    this.descripcionInput = '';
+    this.precioInput = null;
+
+
   }
 
 }
