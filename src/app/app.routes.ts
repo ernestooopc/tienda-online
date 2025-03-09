ab4c8728ffa09d5c33758a27449368688faf3586
Aldo <ernestoo.pc@gmail.com>
2025-03-09 02:43:06 -0500

    feature: add, edit product and component error rutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioProductoComponent } from './formulario-producto/formulario-producto.component';
+import { ErrorComponent } from './error/error.component';
 export const routes: Routes = [
 
   {path:'', component:ListadoProductosComponent},
   {path:'listado', component:ListadoProductosComponent},
   {path:'agregar',component:FormularioProductoComponent},
-  {path:'editar/:id',component:FormularioProductoComponent}
+  {path:'editar/:id',component:FormularioProductoComponent},
+
+  //Ruta comodin para cualquier otra ruta no registrada
+  {path:'**', component:ErrorComponent}
 
 
 
